f05945a6a9acde780443324af668e21e4dff4773
yuki111111 <yuki.koike1111@gmail.com>
2020-01-30 01:00:12 +0900

    Seventeenth commit.
    会員登録情報確認画面で権限名が表示されるよう修正

--- src/main/java/com/koikeya/project1/app/controller/SignUpProcessController.java
+++ src/main/java/com/koikeya/project1/app/controller/SignUpProcessController.java
@@ -51,11 +51,11 @@ public class SignUpProcessController {
      * 権限
      */
  // これがスタティックイニシャライザ?
-    final static Map<Integer, String> SELECT_ROLE =
-      Collections.unmodifiableMap(new LinkedHashMap<Integer, String>() {{
-        put(1, "会員");
-        put(2, "有料会員");
-        put(3, "管理者");
+    final static Map<String, String> SELECT_ROLE =
+      Collections.unmodifiableMap(new LinkedHashMap<String, String>() {{
+        put("1", "会員");
+        put("2", "有料会員");
+        put("3", "管理者");
       }
     });
 
@@ -87,7 +87,7 @@ public class SignUpProcessController {
             return "sign_up";
         }
 
-        model.addAttribute("role", SELECT_ROLE.get(userForm.getId()));
+        model.addAttribute("role", SELECT_ROLE.get(userForm.getRoleId()));
         return "sign_up_confirm";
     }
 }
